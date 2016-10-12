3d61c8a666e1d931a8e5621faad4f7acbdeaf11f
Tony Ta <tonyta.tt@gmail.com>
2016-10-11 21:05:03 -0700

    upload file is now default

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +13,6 @@ func main() {
 	router := gin.Default()
 	router.GET("/", ping)
 	router.POST("/convert", convert)
-	router.POST("/convert_file", convert_file)
 	router.Run()
 }
 
@@ -30,31 +27,6 @@ func ping(c *gin.Context) {
 }
 
 func convert(c *gin.Context) {
-	var json ConvertParams
-
-	validation_err := c.BindJSON(&json)
-	if validation_err != nil {
-		log.Println(validation_err)
-		c.JSON(400, gin.H{"error": "body, to, and from are required params"})
-		return
-	}
-
-	args := []string{"-f", json.From, "-t", json.To}
-
-	cmd := exec.Command("pandoc", args...)
-	cmd.Stdin = strings.NewReader(json.Body)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	cmd_err := cmd.Run()
-
-	if cmd_err != nil {
-		log.Panic(cmd_err)
-	}
-
-	c.JSON(200, gin.H{"format": json.To, "body": out.String()})
-}
-
-func convert_file(c *gin.Context) {
 	var err error
 
 	outFile, err := ioutil.TempFile("", "converted_")
