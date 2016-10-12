62df84ac792d74285cad28ab7d93b43c01d946a0
Tony Ta <tonyta.tt@gmail.com>
2016-10-12 12:06:34 -0700

    allows conversion of docx into html and markdown

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -21,6 +22,47 @@ func ping(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "OK 🐼, Go!"})
 }
 
+func convert(c *gin.Context) {
+	payload, _, err := c.Request.FormFile("payload")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inputFile := createTempFile("source_")
+	defer os.Remove(inputFile.Name())
+	io.Copy(inputFile, payload)
+
+	outputFile := createTempFile("converted_")
+	defer os.Remove(outputFile.Name())
+
+	args := []string{
+		"--standalone",
+		"--from", c.PostForm("from"),
+		"--to", c.PostForm("to"),
+		"--output", outputFile.Name(),
+		inputFile.Name(),
+	}
+
+	err = exec.Command("pandoc", args...).Run()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	data, err := ioutil.ReadAll(outputFile)
+	if err != nil {
+		log.Panic(err)
+	}
+	c.Render(200, render.Data{ContentType: contentType(c.PostForm("to")), Data: data})
+}
+
+func createTempFile(prefix string) *os.File {
+	tempFile, err := ioutil.TempFile("", prefix)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tempFile
+}
+
 func contentType(format string) string {
 	switch format {
 	case "markdown":
@@ -33,34 +75,4 @@ func contentType(format string) string {
 		log.Panic("Unsupported format")
 	}
 	return ""
-}
-
-func convert(c *gin.Context) {
-	var err error
-
-	outFile, err := ioutil.TempFile("", "converted_")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Remove(outFile.Name())
-
-	payload, _, err := c.Request.FormFile("payload")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	args := []string{"-f", c.PostForm("from"), "-t", c.PostForm("to"), "-o", outFile.Name()}
-
-	cmd := exec.Command("pandoc", args...)
-	cmd.Stdin = payload
-	err = cmd.Run()
-	if err != nil {
-		log.Panic(err)
-	}
-
-	data, err := ioutil.ReadAll(outFile)
-	if err != nil {
-		log.Panic(err)
-	}
-	c.Render(200, render.Data{ContentType: contentType(c.PostForm("to")), Data: data})
 }
